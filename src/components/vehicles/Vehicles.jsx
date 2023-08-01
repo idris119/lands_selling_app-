14aa8a1f3ad194fc9395f7f7e2d63f1830bde6e7
Michal <khakaimic4@gmail.com>
2023-08-01 12:52:06 +0300

    vehicles

---
+++ src/components/vehicles/Vehicles.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+
+const Vehicles = () => {
+    const [vehicles, setVehicles] = useState([]);
+
+    useEffect(() => {
+        fetch('')
+    })
+  return (
+    <div>Vehicles</div>
+  )
+}
+
+export default Vehicles
