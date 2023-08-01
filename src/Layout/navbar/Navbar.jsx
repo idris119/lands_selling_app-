fe24f26cb38db9e2e505dd5c6a3c2b75fc4ef35f
Michal <khakaimic4@gmail.com>
2023-08-01 16:35:36 +0300

    navbar

---
+++ src/Layout/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-success mt-3">
+                <div className="container">
+                    <a className="navbar-brand fw-bolder text-white" href="/"><h2>Property Sale</h2></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/Home" className="nav-link active text-white" aria-current="page">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/Properties" className="nav-link active text-white">Properties</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/Payment" className="nav-link active text-white">Payment</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/AddProperty" className="nav-link active text-white">Add Property</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/ApproveProperty" className="nav-link active text-white">Approve Property</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link active dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
+                                <ul className="dropdown-menu bg-success">
+                                    <li><Link to="/Profile" className="dropdown-item text-white" href="#">Profile</Link></li>
+                                    {/* If you had a logout functionality, you can add it here */}
+                                    {/* <li><a className="dropdown-item text-white" href='#' onClick={() => logout()}>logout</a></li> */}
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
--- src/components/vehicles/Vehicles.jsx
+++ src/components/vehicles/Vehicles.jsx
@@ -2,30 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 const Vehicles = () => {
   const [vehicles, setVehicles] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/vehicles')
       .then((r) => r.json())
-      .then((data) => setVehicles(data));
+      .then((data) => setVehicles(data))
+      .catch((error) => setError(error.message)); 
   }, []);
 
   return (
     <div>
       <h1>Vehicle List</h1>
-      <ul>
-        {vehicles.map((vehicle) => (
-          <li key={vehicle.id}>
-            <p>Vehicle Type: {vehicle.vehicle_type}</p>
-            <p>Make: {vehicle.make}</p>
-            <p>Model: {vehicle.model}</p>
-            <p>Colour: {vehicle.colour}</p>
-            <p>Capacity: {vehicle.capacity}</p>
-            <p>Price: {vehicle.price}</p>
-            <p>Image: {vehicle.image}</p>
-            <p>Is Approved: {vehicle.is_approved ? 'Yes' : 'No'}</p>
-          </li>
-        ))}
-      </ul>
+      {error ? (
+        <p>Error occurred while fetching vehicles: {error}</p>
+      ) : (
+        <ul>
+          {vehicles.map((vehicle) => (
+            <li key={vehicle.id}>
+              <p>Vehicle Type: {vehicle.vehicle_type}</p>
+              <p>Make: {vehicle.make}</p>
+              <p>Model: {vehicle.model}</p>
+              <p>Colour: {vehicle.colour}</p>
+              <p>Capacity: {vehicle.capacity}</p>
+              <p>Price: {vehicle.price}</p>
+              <p>Image: {vehicle.image}</p>
+              <p>Is Approved: {vehicle.is_approved ? 'Yes' : 'No'}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
