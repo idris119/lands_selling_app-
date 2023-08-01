9f9f5be5b71dc8659b5d33440c1d30cc234af51f
Michal <khakaimic4@gmail.com>
2023-08-01 18:12:37 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Layout/navbarNavbar';
+import Home from './Home'; 
+import Properties from './Properties'; 
+import Payment from './Payment';
+import AddProperty from './AddProperty'; 
+import ApproveProperty from './ApproveProperty'; 
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/Home" component={Home} />
+          <Route path="/Properties" component={Properties} />
+          <Route path="/Payment" component={Payment} />
+          <Route path="/AddProperty" component={AddProperty} />
+          <Route path="/ApproveProperty" component={ApproveProperty} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
-
-// import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Navbar from './Navbar';
-// import Home from './Home'; 
-// import Properties from './Properties'; 
-// import Payment from './Payment';
-// import AddProperty from './AddProperty'; 
-// import ApproveProperty from './ApproveProperty'; 
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar />
-//         <Switch>
-//           <Route path="/Home" component={Home} />
-//           <Route path="/Properties" component={Properties} />
-//           <Route path="/Payment" component={Payment} />
-//           <Route path="/AddProperty" component={AddProperty} />
-//           <Route path="/ApproveProperty" component={ApproveProperty} />
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
+export default App;
