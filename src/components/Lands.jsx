4ab026e161d7cf18b60fce4298880b74dad4d8b0
ChristopherVuvu <vuvumumo93@gmail.com>
2023-08-01 18:50:54 +0300

    created Landlist code that uses Axios to pick data from backend and map from the list of lands

---
+++ src/components/Lands.jsx
@@ -0,0 +1,29 @@
+// LandsList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function LandsList() {
+    const [lands, setLands] = useState([]);
+
+    useEffect(() => {
+        axios.get('api/lands').then((response) => {
+        setLands(response.data);
+        });
+    }, []);
+
+    return (
+        <div>
+        <h1>List of Lands</h1>
+        {lands.map((land) => (
+            <div key={land.id}>
+            <h2>{land.name}</h2>
+            <p>Location: {land.location}</p>
+            <p>Price: {land.price}</p>
+            {/* Add more details as needed */}
+            </div>
+        ))}
+        </div>
+    );
+}
+
+export default LandsList;
