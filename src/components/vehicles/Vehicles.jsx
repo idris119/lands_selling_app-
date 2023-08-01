4f100b6a762cac7fec2314e4d2ddc35793608560
Michal <khakaimic4@gmail.com>
2023-08-01 13:32:32 +0300

    fetching vehicles

--- src/components/vehicles/Vehicles.jsx
+++ src/components/vehicles/Vehicles.jsx
@@ -1,15 +1,33 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const Vehicles = () => {
-    const [vehicles, setVehicles] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
 
-    useEffect(() => {
-        fetch('')
-    })
+  useEffect(() => {
+    fetch('http://localhost:3000/vehicles')
+      .then((r) => r.json())
+      .then((data) => setVehicles(data));
+  }, []);
+
   return (
-    <div>Vehicles</div>
-  )
-}
+    <div>
+      <h1>Vehicle List</h1>
+      <ul>
+        {vehicles.map((vehicle) => (
+          <li key={vehicle.id}>
+            <p>Vehicle Type: {vehicle.vehicle_type}</p>
+            <p>Make: {vehicle.make}</p>
+            <p>Model: {vehicle.model}</p>
+            <p>Colour: {vehicle.colour}</p>
+            <p>Capacity: {vehicle.capacity}</p>
+            <p>Price: {vehicle.price}</p>
+            <p>Image: {vehicle.image}</p>
+            <p>Is Approved: {vehicle.is_approved ? 'Yes' : 'No'}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Vehicles
+export default Vehicles;
