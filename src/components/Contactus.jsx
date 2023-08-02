c342c8541d3d7e55e7a422f21d3030a263797c70
Charles Onyango <charles.onyango@student.moringaschool.com>
2023-08-02 14:10:31 +0300

    updated

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -18,26 +18,23 @@ const Contactus = () => {
   };
 
   const containerStyle = {
-    width: 'px',
-    margin: '0 auto',
-    padding: '20px',
-    border: '7px navy blue',
-    borderRadius: '5px',
-    backgroundImage: 'url("https://i.pinimg.com/originals/fe/c1/a0/fec1a0579c728c8d26ba30cc08c98832.jpg")', // Replace with the correct image path or URL
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-
+    width: '2000px', // Set the width of the container
+    margin: '0 auto', // Center the container horizontally
+    padding: '20px', // Add some padding to the content inside the container
+    border: '10px solid #ccc', // Add a border around the container
+    borderRadius: '70px', // Round the container edges
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Add a subtle shadow to the container
+    backgroundColor: 'blue', // Set the background color of the container
+    backgroundImage: 'url("https://media.istockphoto.com/id/1365543480/video/contact-us-concept.jpg?s=640x640&k=20&c=G16GzQ4uJKlITww2ArmKNB9UKb1-IT2eyJrUxkUmAIY=")',
   };
-
+  
   return (
     <div style={containerStyle}>
-      <h2 style={{ textAlign: 'center', marginBottom: '50px' }}>Contact US</h2>
+      <h2 style={{ textAlign: 'center', marginBottom: 0 }}>Contact US</h2>
       <h2>PHONE NO: 0722345671</h2>
       <h2>Email: propertyselling@gmail.com</h2>
-
     </div>
   );
-};
+  }  
 
 export default Contactus;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,19 +16,33 @@ const HomePage = () => {
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-    },
-    heading: {
-      fontSize: '40px',
-      color: 'navy',
-      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', 
+      fontFamily: 'Arial, sans-serif', // Replace with your desired font family
     },
     content: {
-      
       margin: '20px',
       padding: '20px',
       borderRadius: '8px',
       backgroundColor: 'rgba(255, 255, 255, 0.8)',
       boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      maxWidth: '500px', // Limit the width to make it more readable on larger screens
+      textAlign: 'center', // Center the content inside the container
+    },
+    heading: {
+      fontSize: '40px',
+      color: 'navy',
+      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+      marginBottom: '20px', // Add some space below the heading
+    },
+    button: {
+      padding: '10px 20px',
+      fontSize: '18px',
+      color: '#fff',
+      backgroundColor: 'navy',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+      transition: 'background-color 0.2s ease',
     },
   };
 
@@ -36,7 +50,11 @@ const HomePage = () => {
     <div style={styles.container}>
       <div style={styles.content}>
         <h1 style={styles.heading}>Welcome to the Property Selling App</h1>
-        
+        <p>
+          This is a platform where you can buy and sell properties easily. 
+          Sign up now to get started!
+        </p>
+      
       </div>
     </div>
   );
