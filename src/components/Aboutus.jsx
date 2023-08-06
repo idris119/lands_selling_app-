50515253adb3e4d61d1a8284d3f56d28a5a1c412
ChristopherVuvu <vuvumumo93@gmail.com>
2023-08-07 00:00:43 +0300

    updated about us page dispaly on css

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -2,27 +2,28 @@ import React from 'react';
 import './AboutUs.css'; // Import the CSS file for styling
 
 function AboutUs() {
-    return (
-        <div className="about-container">
-        <h1 className="about-title">About Us</h1>
-        <p className="about-description">
-            Welcome to our website! Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Nullam dapibus lorem a dui lacinia, quis dapibus ex
-            consectetur. Sed a interdum quam. Proin ut enim ipsum. In at bibendum
-            mauris. Suspendisse potenti. Vestibulum id libero vitae risus
-            malesuada auctor. Etiam gravida, purus ac auctor facilisis, ipsum metus
-            fringilla mauris, nec dignissim dui dui et metus.
-        </p>
-        <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus
-            lorem a dui lacinia, quis dapibus ex consectetur. Sed a interdum quam.
-            Proin ut enim ipsum. In at bibendum mauris. Suspendisse potenti.
-            Vestibulum id libero vitae risus malesuada auctor. Etiam gravida, purus
-            ac auctor facilisis, ipsum metus fringilla mauris, nec dignissim dui
-            dui et metus.
-        </p>
-        </div>
-    );
+  return (
+    <div className="about-container">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-description">
+        Welcome to our website! Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nullam dapibus lorem a dui lacinia, quis dapibus ex
+        consectetur. Sed a interdum quam. Proin ut enim ipsum. In at bibendum
+        mauris. Suspendisse potenti. Vestibulum id libero vitae risus
+        malesuada auctor. Etiam gravida, purus ac auctor facilisis, ipsum metus
+        fringilla mauris, nec dignissim dui dui et metus.
+      </p>
+      <p className="about-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus
+        lorem a dui lacinia, quis dapibus ex consectetur. Sed a interdum quam.
+        Proin ut enim ipsum. In at bibendum mauris. Suspendisse potenti.
+        Vestibulum id libero vitae risus malesuada auctor. Etiam gravida, purus
+        ac auctor facilisis, ipsum metus fringilla mauris, nec dignissim dui
+        dui et metus.
+      </p>
+      <a href="/contact" className="about-link">Contact Us</a>
+    </div>
+  );
 }
 
 export default AboutUs;
