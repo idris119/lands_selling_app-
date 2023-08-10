a97b86e961a770a4e95ce868868aac0d32b91e4c
Timothy <todongoonditi@gmail.com>
2023-08-10 23:14:42 +0300

    updated peyment, navbar and authcontext

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -34,8 +34,177 @@
 // };
 
 // export default Payment;
+// import React, { useState } from "react";
+// import { useParams } from 'react-router-dom';
+// import './Payment.css';
+
+// const PaymentForm = () => {
+//   const { id } = useParams();
+
+//   const [propertyId, setPropertyId] = useState(id);
+//   const [amount, setAmount] = useState("");
+//   const [paymentMethod, setPaymentMethod] = useState("mpesa");
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const [customerId, setCustomerId] = useState("");
+//   const [contactNumber, setContactNumber] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [location, setLocation] = useState("");
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     const customerName = `${firstName} ${lastName}`;
+
+//     const formData = new FormData();
+//     formData.append("propertyId", propertyId);
+//     formData.append("amount", amount);
+//     formData.append("paymentMethod", paymentMethod);
+//     formData.append("customerName", customerName);
+//     formData.append("customerId", customerId);
+//     formData.append("contactNumber", contactNumber);
+//     formData.append("email", email);
+//     formData.append("location", location);
+
+//     try {
+//       const response = await fetch(`/make-${paymentMethod}-payment`, {
+//         method: "POST",
+//         body: formData,
+//       });
+//       const data = await response.json();
+//       console.log(data);
+//     } catch (error) {
+//       console.error("Error making payment:", error);
+//     }
+//     {
+//       Swal.fire(
+//           'error',
+//           response.error,
+//           'error'
+//         )
+//   }
+//   else if(response.success)
+//   { 
+//       nav("/")
+//       Swal.fire(
+//           'success',
+//           response.success,
+//           'success'
+//         )
+//         setonChange(!onChange)
+
+
+//   }
+//   else{
+//       Swal.fire(
+//           'Error',
+//           "Something went wrong",
+//           'error'
+//         )
+//   }
+
+// })
+//   };
+
+//   return (
+//     <div className="payment-form-container">
+//       <h1 className="form-title">Property Purchase</h1>
+//       <form className="payment-form" onSubmit={handleSubmit}>
+//         <label htmlFor="propertyId">Property ID:</label>
+//         <input
+//           type="text"
+//           id="propertyId"
+//           value={propertyId}
+//           onChange={(e) => setPropertyId(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="amount">Amount:</label>
+//         <input
+//           type="number"
+//           id="amount"
+//           value={amount}
+//           onChange={(e) => setAmount(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="paymentMethod">Payment Method:</label>
+//         <select
+//           id="paymentMethod"
+//           value={paymentMethod}
+//           onChange={(e) => setPaymentMethod(e.target.value)}
+//           required
+//         >
+//           <option value="mpesa">M-Pesa</option>
+//           <option value="visa">Visa</option>
+//           <option value="paypal">PayPal</option>
+//         </select>
+
+//         <label htmlFor="firstName">First Name:</label>
+//         <input
+//           type="text"
+//           id="firstName"
+//           value={firstName}
+//           onChange={(e) => setFirstName(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="lastName">Last Name:</label>
+//         <input
+//           type="text"
+//           id="lastName"
+//           value={lastName}
+//           onChange={(e) => setLastName(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="customerId">Customer ID:</label>
+//         <input
+//           type="text"
+//           id="customerId"
+//           value={customerId}
+//           onChange={(e) => setCustomerId(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="contactNumber">Contact Number:</label>
+//         <input
+//           type="text"
+//           id="contactNumber"
+//           value={contactNumber}
+//           onChange={(e) => setContactNumber(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="email">Email Address:</label>
+//         <input
+//           type="email"
+//           id="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           required
+//         />
+
+//         <label htmlFor="location">Location:</label>
+//         <input
+//           type="text"
+//           id="location"
+//           value={location}
+//           onChange={(e) => setLocation(e.target.value)}
+//           required
+//         />
+
+//         <button className="submit-button" type="submit">Submit Payment</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default PaymentForm;
+
 import React, { useState } from "react";
 import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2'; // Import Swal for displaying alerts
 import './Payment.css';
 
 const PaymentForm = () => {
@@ -73,6 +242,15 @@ const PaymentForm = () => {
       });
       const data = await response.json();
       console.log(data);
+
+      if (data.error) {
+        Swal.fire('error', data.error, 'error');
+      } else if (data.success) {
+        Swal.fire('success', data.success, 'success');
+        // Redirect or update state as needed
+      } else {
+        Swal.fire('Error', 'Something went wrong', 'error');
+      }
     } catch (error) {
       console.error("Error making payment:", error);
     }
@@ -82,8 +260,10 @@ const PaymentForm = () => {
     <div className="payment-form-container">
       <h1 className="form-title">Property Purchase</h1>
       <form className="payment-form" onSubmit={handleSubmit}>
+        {/* Input fields */}
+        {/* ... */}
         <label htmlFor="propertyId">Property ID:</label>
-        <input
+<input
           type="text"
           id="propertyId"
           value={propertyId}
@@ -165,7 +345,7 @@ const PaymentForm = () => {
           onChange={(e) => setLocation(e.target.value)}
           required
         />
-
+        
         <button className="submit-button" type="submit">Submit Payment</button>
       </form>
     </div>
